7c2c232574ab98e71c95045ec48dabffa6fac41b
LilianKalkman <liliankalkman@hotmail.com>
2018-03-16 15:03:06 +0100

    show connected to state

--- src/containers/Products/show_product.js
+++ src/containers/Products/show_product.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const ShowProduct = () => {
-  return(
-    <div>
-      Product
-    </div>
-  );
+class ShowProduct extends Component {
+  componentDidMount(){
+    console.log(this.props);
+    console.log(this.props.products[this.props.match.params.productId]);
+  }
+
+  render() {
+    const productinfo = this.props.products[this.props.match.params.productId];
+    const product = Object.keys(productinfo).map((key) => {
+      return <li>{productinfo[key]}</li>
+    });
+    return(
+      <div>
+      {product}
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
