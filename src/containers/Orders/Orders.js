cb3099cafb7a6b962c197618fb882242ac77c94b
LilianKalkman <liliankalkman@hotmail.com>
2018-03-20 10:26:23 +0100

    first set up orders component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const Orders = () => {
-  return(
-    <div>
-      Orders
-    </div>
-  );
+class Orders extends Component {
+  render(){
+    const orders = Object.keys(this.props.orders);
+    const mappedorders = orders.map(item => {
+      return <li>{item.id}</li>
+    });
+    return (
+      <div>
+        Orders
+        {orders}
+        {mappedorders}
+      </div>
+    );
+  }
 }
 
-export default Orders;
+const mapStateToProps = (state) => {
+  return {
+    orders: state.orders
+  };
+};
+
+export default connect(mapStateToProps)(Orders);
