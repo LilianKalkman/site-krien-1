9584175db2c2c9606cc950ded1d26b09d5f7f500
LilianKalkman <liliankalkman@hotmail.com>
2018-04-03 13:13:16 +0200

    ordersum

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Checkout.css';
+import { formatPrice } from '../../helpers';
 
 class Checkout extends Component {
   render(){
     return (
       <div className="checkout">
+        <div className="ordersum">
+          <h4>Your Order:</h4>
+          {this.props.orders.map(order => {
+            return <h6>{order.name}</h6>
+          })}
+          <h4>Total:</h4>
+          <p>{formatPrice(this.props.price)}</p>
+        </div>
         <form action="">Checkout-form: order summary + data invullen + place order button die data naar server stuurt.</form>
       </div>
     );
   }
 }
 
-export default Checkout;
+const mapStateToProps = (state) => {
+  return {
+    orders: state.orders,
+    price: state.totalPrice
+  };
+};
+
+
+export default connect(mapStateToProps)(Checkout);
